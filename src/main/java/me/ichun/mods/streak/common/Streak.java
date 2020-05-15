92c9cfbcd4ac237d143f03ae160e8ef8291577f1
Kay Vin Tee <ohai.iChun@gmail.com>
2020-05-15 11:34:03 +0800

    Of course some files are left out.

---
+++ src/main/java/me/ichun/mods/streak/common/Streak.java
@@ -0,0 +1,121 @@
+package me.ichun.mods.streak.common;
+
+import me.ichun.mods.ichunutil.client.tracker.ClientEntityTracker;
+import me.ichun.mods.streak.common.core.Config;
+import me.ichun.mods.streak.common.core.EventHandler;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+@Mod(Streak.MOD_ID)
+public class Streak
+{
+    public static final String MOD_ID = "streak";
+    public static final String MOD_NAME = "Streak";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public static Config config;
+
+    public static EventHandler eventHandler;
+
+    public Streak() //TODO add iChunUtil dep. Version checker???
+    {
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+            config = new Config().init();
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+            ClientEntityTracker.init(FMLJavaModLoadingContext.get().getModEventBus());
+            MinecraftForge.EVENT_BUS.register(eventHandler = new EventHandler());
+            ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> me.ichun.mods.ichunutil.client.core.EventHandlerClient::getConfigGui);
+        });
+        DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.log(Level.ERROR, "You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event)
+    {
+        unpackFlavours();
+    }
+
+    private void unpackFlavours()
+    {
+        File streakDir = new File(FMLPaths.MODSDIR.get().toFile(), "/Streak Flavours");
+        if(!streakDir.exists() && streakDir.mkdirs())
+        {
+            try(InputStream in = Streak.class.getResourceAsStream("/flavours.zip"); ZipInputStream zipStream = new ZipInputStream(in))
+            {
+                ZipEntry entry = null;
+
+                int extractCount = 0;
+
+                while((entry = zipStream.getNextEntry()) != null)
+                {
+                    File file = new File(streakDir, entry.getName());
+                    if(file.exists() && file.length() > 3L)
+                    {
+                        continue;
+                    }
+                    FileOutputStream out = new FileOutputStream(file);
+
+                    byte[] buffer = new byte[8192];
+                    int len;
+                    while((len = zipStream.read(buffer)) != -1)
+                    {
+                        out.write(buffer, 0, len);
+                    }
+                    out.close();
+
+                    extractCount++;
+                }
+
+                if(extractCount > 0)
+                {
+                    Streak.LOGGER.info("Extracted flavours from mod zip: {}", extractCount);
+                }
+            }
+            catch(NullPointerException | IOException e)
+            {
+                Streak.LOGGER.warn("Failed to extract streak flavours from mod jar");
+                e.printStackTrace();
+            }
+        }
+
+        if(streakDir.exists())
+        {
+            for(File file : streakDir.listFiles())
+            {
+                if(file.getName().endsWith(".png"))
+                {
+                    try
+                    {
+                        BufferedImage img = ImageIO.read(file);
+                        if(img != null)
+                        {
+                            eventHandler.flavours.add(new EventHandler.FlavourInfo(file.getName().substring(0, file.getName().length() - 4).toLowerCase(), img));
+                        }
+                    }
+                    catch(IOException e)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/me/ichun/mods/streak/common/core/Config.java
@@ -0,0 +1,107 @@
+package me.ichun.mods.streak.common.core;
+
+import me.ichun.mods.ichunutil.common.config.ConfigBase;
+import me.ichun.mods.ichunutil.common.config.annotations.CategoryDivider;
+import me.ichun.mods.ichunutil.common.config.annotations.Prop;
+import me.ichun.mods.streak.common.Streak;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.ResourceLocationException;
+import net.minecraftforge.fml.config.ModConfig;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class Config extends ConfigBase
+{
+    //TODO localise these
+    @CategoryDivider(name = "clientOnly")
+    @Prop(min = 5)
+    public int streakTime = 100;
+
+    @Prop(min = 0, max = 100)
+    public int streakOpacity = 100;
+
+    @Prop(min = 0)
+    public int sprintTrail = 6;
+
+    @Prop
+    public boolean renderInFirstPerson = false;
+
+    @Prop(validator = "attachValidator")
+    public List<String> attachTo = new ArrayList<String>(){{
+        add("minecraft:player");
+    }};
+
+    @Prop(validator = "flavorValidator")
+    public List<String> setFlavors = new ArrayList<>(); //we're using "flavor" because ehmerican Enghlessh
+
+    public HashSet<ResourceLocation> attachments = new HashSet<>();
+    public HashMap<String, String> setFlavourMap = new HashMap<>();
+
+    public boolean attachValidator(Object o)
+    {
+        if(o instanceof String)
+        {
+            try
+            {
+                new ResourceLocation(((String)o));
+                return true;
+            }
+            catch(ResourceLocationException ignored){}
+        }
+        return false;
+    }
+
+    public boolean flavorValidator(Object o)
+    {
+        return o instanceof String && ((String)o).split(":").length == 2;
+    }
+
+    @Nonnull
+    @Override
+    public String getModId()
+    {
+        return Streak.MOD_ID;
+    }
+
+    @Nonnull
+    @Override
+    public String getConfigName()
+    {
+        return Streak.MOD_NAME;
+    }
+
+    @Nonnull
+    @Override
+    public ModConfig.Type getConfigType()
+    {
+        return ModConfig.Type.CLIENT;
+    }
+
+    @Override
+    public void onConfigLoaded()
+    {
+        Minecraft.getInstance().execute(this::parseConfig);
+    }
+
+    public void parseConfig()
+    {
+        attachments.clear();
+        for(String s : attachTo)
+        {
+            attachments.add(new ResourceLocation(s));
+        }
+
+        setFlavourMap.clear();
+        for(String s : setFlavors)
+        {
+            String[] split = s.split(":");
+            setFlavourMap.put(split[0], split[1].toLowerCase());
+        }
+    }
+
+}
